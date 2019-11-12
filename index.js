f14da1d81ca21724aa7f48016f5e387bfd2eaad5
Henning Schaar <henning.schaar@gmail.com>
2019-11-12 15:09:05 +0100

    Added error handling for request

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ let originDistances = [];
 let destinationDistances = [];
 const stationLocation = "Riedstadt-Goddelau";
 
-
-
 function perform() {
 
     request('https://www.zugfinder.de/js/json_kbs.php?kbs=650', function (error, response, body) {
-
+        if(error != null){
+            handleError(error);
+            return;
+        }
         trainData = JSON.parse(body); //Parse incoming JSON
         time = trainData.zeit;
         trains = trainData.array;
@@ -22,33 +23,35 @@ function perform() {
             return train;
         })
 
-        // Add distance between start and destination points using Googles Distance Matrix API wrapped with the np google-distance.
+        addDistanceData();
+    });
+}
 
-        let origins = [stationLocation];
-        let destinations = [stationLocation];
-        for (let i = 0; i < trains.length; i++) {
-            origins[i + 1] = trains[i].lauf[0]
-            destinations[i + 1] = trains[i].lauf[1]
+function addDistanceData() {
+    // Add distance between start and destination points using Googles Distance Matrix API wrapped with the np google-distance.
+    let origins = [stationLocation];
+    let destinations = [stationLocation];
+    for (let i = 0; i < trains.length; i++) {
+        origins[i + 1] = trains[i].lauf[0]
+        destinations[i + 1] = trains[i].lauf[1]
+    }
+
+    let apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${
+        escape(origins.join("|").replace(/ /g, "+"))
+        }&destinations=${
+        escape(destinations.join("|").replace(/ /g, "+"))
+        }&key=AIzaSyBnzzUkE5XQdRW_vJ41S1RL9khjFrJWWh4`;
+
+    request(apiUrl, (err, resp, body) => {
+
+        distanceData = JSON.parse(body);
+
+        if (distanceData.status == "OK") {
+            parseDistanceData(distanceData);
         }
+        else { handleError("API Request failed." + "\n" + distanceData); }
 
-        let apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${
-            escape(origins.join("|").replace(/ /g, "+"))
-            }&destinations=${
-            escape(destinations.join("|").replace(/ /g, "+"))
-            }&key=AIzaSyBnzzUkE5XQdRW_vJ41S1RL9khjFrJWWh4`;
-
-        request(apiUrl, (err, resp, body) => {
-
-            distanceData = JSON.parse(body);
-
-            if (distanceData.status == "OK") {
-                parseDistanceData(distanceData);
-            }
-            else { console.log(distanceData.stat); }
-
-            setTimeout(perform, 10000);
-        });
-
+        setTimeout(perform, 10000);
     });
 }
 
@@ -66,10 +69,12 @@ function parseDistanceData(distanceData) {
         train.zurueckGelegt = distanceToStation / train.strecke;
         train.zurueckGelegt = train.zurueckGelegt.toFixed(3);
     });
+    console.log(trains);
+}
 
-    console.log(originDistances);
-    console.log(destinationDistances);
-    console.log(trainData);
+function handleError(error) {
+    console.log(error);
+    setTimeout(perform, 1000);
 }
 
 perform();
