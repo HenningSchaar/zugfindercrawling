b85b387fb6f7c2d8300c8ec8b065bf0d10876e58
Henning Schaar <henning.schaar@gmail.com>
2019-11-12 16:50:41 +0100

    Not currently working. Added debugging

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const request = require('request');
 const escape = require('remove-accents');
 
-var PORT = 9091;
-var HOST = '127.0.0.1';
-var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
+const limit = [0, 1000];
 
-let originDistances = [];
-let destinationDistances = [];
-const stationLocation = "Riedstadt-Goddelau";
+const PORT = 9091;
+const HOST = '127.0.0.1';
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+
+const stationLocation = "Hähnlein-Alsbach";
 
 function perform() {
 
@@ -20,6 +21,7 @@ function perform() {
         trainData = JSON.parse(body); //Parse incoming JSON
         trains = trainData.array; //convert train data to an array of trains
         trains.shift(); //remove first entry which for some reason is always empty
+        trains = trains.slice(0, 10);
 
         // Replace string which describes "lauf" with an array of the two train stations in question.
         trains = trains.map(train => {
@@ -60,19 +62,27 @@ function addDistanceData() {
         if (distanceData.status == "OK") {
             parseDistanceData(distanceData);
         }
-        else { handleError("API Request failed." + "\n" + distanceData); }
+        else { handleError("API Request failed." + "\n" + distanceData.status); }
 
-        setTimeout(perform, 10000);
+        setTimeout(perform, 5000);
     });
 }
 
 function parseDistanceData(distanceData) {
     //console.log(distanceData.rows[0].elements)
+    let originDistances = [];
+    let destinationDistances = [];
     distanceData.rows[0].elements.forEach(originDistanceEntry => {
-        originDistances.push(originDistanceEntry.distance.value);
+        if(originDistanceEntry.status == "OK"){
+            originDistances.push(originDistanceEntry.distance.value);
+        }
+        else{console.log("Failed to read origin distance entry: " + console.log(originDistanceEntry))};
     });
     distanceData.rows.forEach(destinationDistanceEntry => {
-        destinationDistances.push(destinationDistanceEntry.elements[0].distance.value);
+        if(destinationDistanceEntry.status == "OK"){
+            destinationDistances.push(destinationDistanceEntry.distance.value);
+        }
+        else{console.log("Failed to read destination distance entry: " + console.log(destinationDistanceEntry))};
     });
     trains.forEach((train, i) => {
         distanceToStation = originDistances[i + 1];
@@ -80,24 +90,34 @@ function parseDistanceData(distanceData) {
         train.zurueckGelegt = distanceToStation / train.strecke;
         train.zurueckGelegt = train.zurueckGelegt.toFixed(3);
     });
-    console.log(trains);
+    //console.log(trains);
     sendDataToMax(trains);
 }
 
 function sendDataToMax(trains) {
+    let trainmessages = 0;
     trains.forEach((train, i) => {
-        message = train.zugnr.replace(" ", "") + " "+ train.strecke + " " + train.zurueckGelegt
-        client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
-            if (err) throw err;
-        });
+        message = train.zugnr.replace(" ", "") + " " + train.strecke + " " + train.zurueckGelegt;
+        if(limit[0] < train.ypos < limit[1]){
+            trainmessages++;
+            client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+                if (err) throw err;
+            });
+        }
     });
+    if (trainmessages > 0){
+        console.log("Sent " + trainmessages + " trains to max.")
+    }else{
+        console.log("\nNo trains nearby \n");
+        trains.forEach(train => {
+            console.log(train.zugnr + " " + train.ypos);
+        });
+    };
 }
 
 function handleError(error) {
     console.log(error);
     setTimeout(perform, 1000);
 }
-
-
 
 perform();
